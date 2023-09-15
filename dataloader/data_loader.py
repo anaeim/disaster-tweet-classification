2b3cff247d91e4e9de7363f54f7f48dbf30fe464
ali <ali.naeim.gdrive@gmail.com>
2023-09-15 15:46:16 -0600

    add documentation for data_manipulation.py

--- dataloader/data_loader.py
+++ dataloader/data_loader.py
@@ -2,6 +2,12 @@ from pathlib import Path
 import pandas as pd
 
 def load():
+    """load the data from the data_path provided on config.py
+
+    Returns
+    -------
+    pandas.DataFrame
+    """
     data_path = Path().cwd() / "data" / "train.csv"
     # print(data_path)
     df = pd.read_csv(data_path)
