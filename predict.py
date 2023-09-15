b4243d9bde48b7d8aa861346c62b8854a6c2eed0
ali <ali.naeim.gdrive@gmail.com>
2023-09-15 14:22:13 -0600

    modify predict() to be compatible with models/bert_models.py and to pass the training data through model.fit()

--- predict.py
+++ predict.py
@@ -8,12 +8,18 @@ def main():
     args = parse_args()
     df = data_loader.load()
     df = data_manipulation.load_data_manipulation(df)
-    model = {
-        'bert_model': models.bert_model
+    train_data, valid_data = train_test_split(df, test_size=args.validation_split)
+    x_train = train_data['text']
+    x_valid = valid_data['text']
+    y_train = train_data['target']
+    y_valid = valid_data['target']
+
+    Model = {
+        'bert_model': models.bert_model.TweetClassifier
     }[args.ml-model]
 
-    model = model.TweetClassifier(df, args)
-    model.fit()
+    model = Model()
+    model.fit(x_train, y_train, args)
 
 if __name__ == "__main__":
     main()
